c5c6dd5872d9dcc52121d752ce9f5c0c2be90d6e
aduugen <z_semail@163.com>
2025-04-14 07:55:13 +0800

    数据表重构

--- src/api/orderReturnApply.js
+++ src/api/orderReturnApply.js
@@ -2,7 +2,7 @@ import request from '@/utils/request'
 
 export function getOrderReturnApplyStatistic() {
   return request({
-    url: '/returnApply/statusStatistic',
+    url: '/afterSale/statusStatistic',
     method: 'get'
   })
 }
